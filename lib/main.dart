75a048ab1e2eb08a09e388f9087150190a16e2dc
Taufiq Sejati <taufiq_sejati@rocketmail.com>
2022-03-30 17:10:17 +0700

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:infinite_list_pagination/model/article_data.dart';
 import 'package:infinite_list_pagination/model/passenger_data.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -42,14 +44,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPage = 0;
-  late int totalPages;
+  int totalPages = 100;
 
-  List<Passenger> passengers = [];
+  // List<Passenger> passengers = [];
+  List<Article> articles = [];
   final RefreshController refreshController =
       RefreshController(initialRefresh: true);
   Future<bool> getPassengerData({bool isRefresh = false}) async {
+    // await Future.delayed(Duration(milliseconds: 2000));
+
     if (isRefresh) {
       currentPage = 0;
+      debugPrint('0 $currentPage ');
     } else {
       if (currentPage >= totalPages) {
         refreshController.loadNoData();
@@ -57,24 +63,29 @@ class _HomePageState extends State<HomePage> {
       }
     }
 
-    final Uri uri = Uri.parse(
-        "https://api.instantwebtools.net/v1/passenger?page=$currentPage&size=10");
-    final response = await http.get(uri);
+    // final Uri uri = Uri.parse(
+    //     "https://api.instantwebtools.net/v1/passenger?page=$currentPage&size=10");
+    final Uri uri2 = Uri.parse(
+        "http://114.141.50.163:8080/api/listArticles?page=$currentPage&size=2");
+    final response = await http.get(uri2);
 
     if (response.statusCode == 200) {
-      final result = welcomeFromJson(response.body);
+      // final result = welcomeFromJson(response.body);
+      final result = articleFromJson(response.body);
       if (isRefresh) {
-        passengers = result.data!;
+        debugPrint('1 $currentPage');
+        articles = result.articles;
       } else {
-        passengers.addAll(result.data!);
+        articles.addAll(result.articles);
       }
 
       currentPage++;
 
-      totalPages = result.totalPages!;
+      totalPages = result.totalPages;
 
-      print(response.body);
+      // debugPrint("${currentPage} $totalPages");
       setState(() {});
+      refreshController.loadComplete();
       return true;
     } else {
       return false;
@@ -89,9 +100,31 @@ class _HomePageState extends State<HomePage> {
         ),
         body: SmartRefresher(
           controller: refreshController,
+          header: const WaterDropHeader(),
+          // footer: CustomFooter(
+          //   builder: (context, mode) {
+          //     Widget body;
+          //     if (mode == LoadStatus.idle) {
+          //       body = const Text("pull up load");
+          //     } else if (mode == LoadStatus.loading) {
+          //       body = const CupertinoActivityIndicator();
+          //     } else if (mode == LoadStatus.failed) {
+          //       body = const Text("Load Failed!Click retry!");
+          //     } else if (mode == LoadStatus.canLoading) {
+          //       body = const Text("release to load more");
+          //     } else {
+          //       body = const Text("No more Data");
+          //     }
+          //     return SizedBox(
+          //       height: 55.0,
+          //       child: Center(child: body),
+          //     );
+          //   },
+          // ),
           enablePullUp: true,
           onRefresh: () async {
             final result = await getPassengerData(isRefresh: true);
+            debugPrint('2 $currentPage $totalPages $result');
             if (result) {
               refreshController.refreshCompleted();
             } else {
@@ -100,26 +133,77 @@ class _HomePageState extends State<HomePage> {
           },
           onLoading: () async {
             final result = await getPassengerData();
+            debugPrint('$currentPage $totalPages');
             if (result) {
+              debugPrint(articles.toString());
               refreshController.loadComplete();
+            } else if (currentPage >= totalPages) {
+              debugPrint('error 0');
+              refreshController.loadNoData();
             } else {
               refreshController.loadFailed();
             }
+            //          if (currentPage >= totalPages) {
+            //   refreshController.loadNoData();
+            //   return false;
+            // }
           },
-          child: ListView.separated(
-            itemBuilder: (context, index) {
-              final passenger = passengers[index];
-              return ListTile(
-                title: Text(passenger.name.toString()),
-                subtitle:
-                    Text(passenger.airline!.map((e) => e.country).toString()),
-                trailing:
-                    Text(passenger.airline!.map((e) => e.name).toString()),
-              );
-            },
-            separatorBuilder: (context, index) => Divider(),
-            itemCount: passengers.length,
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            // padding: const EdgeInsets.only(bottom: 16),
+            children: [
+              GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 0.7,
+                // childAspectRatio: 1,
+                padding: const EdgeInsets.all(15),
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: articles
+                    .map((e) => Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                                color: Colors.blueAccent.shade100,
+                                width: 0.75)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 125,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          'http://114.141.50.163:8080/uploads/news/${e.image}'))),
+                            ),
+                            Text(e.title),
+                            Text(e.category),
+                            Text(e.createdAt.toString()),
+                          ],
+                        )))
+                    .toList(),
+              ),
+            ],
           ),
+
+          // child: ListView.separated(
+          //   shrinkWrap: true,
+          //   itemBuilder: (context, index) {
+          //     final article = articles[index];
+          //     return ListTile(
+          //       title: Text(article.title.toString()),
+          //       subtitle: Text(article.category.toString()),
+          //       trailing: Text(article.image.toString()),
+          //     );
+          //   },
+          //   separatorBuilder: (context, index) => Divider(),
+          //   itemCount: articles.length,
+          // ),
         ));
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:infinite_list_pagination/main.dart';
+import 'package:infinite_list_pagination/mainnn.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
